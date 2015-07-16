c3df7f111c1f22a562da125ddc69510e0e0260b3
yansong li <857367901@qq.com>
2015-07-16 18:58:21 -0400

    Tableview

--- GitPocket/GitPocket/Model/Event.swift
+++ GitPocket/GitPocket/Model/Event.swift
@@ -13,27 +13,85 @@ class Event {
   // https://developer.github.com/v3/activity/events/types/
   enum EventType: String {
     case CreateEvent = "CreateEvent"
+    case CommitCommentEvent = "CommitCommmentEvent"
     case DeleteEvent = "DeleteEvent"
-    case WatchEvent = "WatchEvent"
+    case DeploymentEvent = "DeploymentEvent"
+    case DeploymentStatusEvent = "DeploymentStatusEvent"
+    case DownloadEvent = "DownloadEvent"
     case FollowEvent = "FollowEvent"
     case ForkEvent = "ForkEvent"
+    case ForkApplyEvent = "ForkApplyEvent"
+    case GistEvent = "GistEvent"
+    case GollumEvent = "GollumEvent"
+    case IssueCommentEvent = "IssueCommentEvent"
+    case IssuesEvent = "IssuesEvent"
     case MemberEvent = "MemberEvent"
+    case MembershipEvent = "MembershipEvent"
+    case PageBuildEvent = "PageBuildEvent"
+    case PublicEvent = "PublicEvent"
+    case PullRequestEvent = "PullRequestEvent"
+    case PullRequestReviewCommentEvent = "PullRequestReviewCommentEvent"
+    case PushEvent = "PushEvent"
+    case ReleaseEvent = "ReleaseEvent"
+    case RepositoryEvent = "RepositoryEvent"
+    case StatusEvent = "StatusEvent"
+    case TeamAddEvent = "TeamAddEvent"
+    case WatchEvent = "WatchEvent"
     
     init?(event:String) {
       switch event {
-        case "CreatEvent":
+        case "CreateEvent":
           self = .CreateEvent
+        case "CommitCommentEvent":
+          self = .CommitCommentEvent
         case "DeleteEvent":
           self = .DeleteEvent
-        case "WatchEvent":
-          self = .WatchEvent
+        case "DeploymentEvent":
+          self = .DeploymentEvent
+        case "DeploymentStatusEvent":
+          self = .DeploymentStatusEvent
+        case "DownloadEvent":
+          self = .DownloadEvent
         case "FollowEvent":
           self = .FollowEvent
         case "ForkEvent":
           self = .ForkEvent
+        case "ForkApplyEvent":
+          self = .ForkApplyEvent
+        case "GistEvent":
+          self = .GistEvent
+        case "GollumEvent":
+          self = .GollumEvent
+        case "IssueCommentEvent":
+          self = .IssueCommentEvent
+        case "IssuesEvent":
+          self = .IssuesEvent
         case "MemberEvent":
           self = .MemberEvent
+        case "MembershipEvent":
+          self = .MembershipEvent
+        case "PageBuildEvent":
+          self = .PageBuildEvent
+        case "PublicEvent":
+          self = .PublicEvent
+        case "PullRequestEvent":
+          self = .PullRequestEvent
+        case "PullRequestReviewCommentEvent":
+          self = .PullRequestReviewCommentEvent
+        case "PushEvent":
+          self = .PushEvent
+        case "ReleaseEvent":
+          self = .ReleaseEvent
+        case "RepositoryEvent":
+          self = .RepositoryEvent
+        case "StatusEvent":
+          self = .StatusEvent
+        case "TeamAddEvent":
+          self = .TeamAddEvent
+        case "WatchEvent":
+          self = .WatchEvent
         default:
+          print("There has \(event)")
           return nil
       }
     }
---
+++ GitPocket/GitPocket/View/GithubFeedCell.swift
@@ -0,0 +1,9 @@
+//
+//  GithubFeedCell.swift
+//  GitPocket
+//
+//  Created by yansong li on 2015-07-15.
+//  Copyright © 2015 yansong li. All rights reserved.
+//
+
+import Foundation
