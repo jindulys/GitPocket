89589003149289f6d8944330fe0cb6456c72bf5c
yansong li <857367901@qq.com>
2015-07-25 11:36:12 -0400

    Fix avatarview priority

--- GitPocket/GitPocket/View/GithubFeedCell.swift
+++ GitPocket/GitPocket/View/GithubFeedCell.swift
@@ -114,6 +114,7 @@ class GithubFeedCell: UITableViewCell
     let avatarWidthConstraint = avatarView.widthAnchor.constraintEqualToConstant(60)
     avatarWidthConstraint.identifier = "avatarWidthConstraint"
     let avatarHeightConstraint = avatarView.heightAnchor.constraintEqualToConstant(60.0)
+    avatarHeightConstraint.priority = 999
     avatarHeightConstraint.identifier = "avatarHeightConstraint"
     // Action Label Constraints
     let actionLeadingConstraint = actionLabel.leadingAnchor.constraintEqualToAnchor(contentView.leadingAnchor, constant: 15.0)
